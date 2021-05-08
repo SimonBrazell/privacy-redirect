738703f1cc50066aa2a84a7e288f20809c853c87
alefvanoon <53198048+alefvanoon@users.noreply.github.com>
2021-05-08 03:04:52 -0700

    Update google-search.js

--- src/assets/javascripts/helpers/google-search.js
+++ src/assets/javascripts/helpers/google-search.js
@@ -11,6 +11,7 @@ const redirects = [
   { link: "https://search.disroot.org", q: "/" },
   { link: "https://searx.tuxcloud.net", q: "/" },
   { link: "https://searx.ninja", q: "/" },
+  { link: "https://sx.alefvanoon.xyz", q: "/" },
   { link: "https://tromland.org/searx", q: "/search" },
   { link: "https://engine.presearch.org", q: "/search" },
   { link: "https://searx.silkky.cloud", q: "/" },
