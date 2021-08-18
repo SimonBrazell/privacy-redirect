ae8015cff8daa4088cfd6ac6591f032bdfc0dc26
walking-octopus <46994949+walking-octopus@users.noreply.github.com>
2021-08-18 09:28:46 +0000

    Add lingva.ml

--- src/assets/javascripts/helpers/google-translate.js
+++ src/assets/javascripts/helpers/google-translate.js
@@ -1,6 +1,6 @@
 const targets = ["translate.google.com", "translate.google.no"];
 
-const redirects = ["https://translate.metalune.xyz"];
+const redirects = ["https://translate.metalune.xyz", "https://lingva.ml"];
 
 export default {
   targets,
