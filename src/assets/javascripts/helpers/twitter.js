b18a24a65522b63b755fe5d58fe6620130ce74a1
moom0o <48740106+moom0o@users.noreply.github.com>
2021-10-09 15:51:06 -0400

    Add nitter.moomoo.me & onion mirror

--- src/assets/javascripts/helpers/twitter.js
+++ src/assets/javascripts/helpers/twitter.js
@@ -32,10 +32,12 @@ const redirects = [
   "https://nitter.vxempire.xyz",
   "https://nitter.unixfox.eu",
   "https://bird.trom.tf",
+  "https://nitter.moomoo.me",
   "http://3nzoldnxplag42gqjs23xvghtzf6t6yzssrtytnntc6ppc7xxuoneoad.onion",
   "http://nitter.l4qlywnpwqsluw65ts7md3khrivpirse744un3x7mlskqauz5pyuzgqd.onion",
   "http://nitterlgj3n5fgwesu3vxc5h67ruku33nqaoeoocae2mvlzhsu6k7fqd.onion",
   "http://npf37k3mtzwxreiw52ccs5ay4e6qt2fkcs2ndieurdyn2cuzzsfyfvid.onion",
+  "http://nitterrrs6bbcba2bxjviwxzzapkhuuelljtig2ku2rxasweckxxxhid.onion",
 ];
 
 export default {
