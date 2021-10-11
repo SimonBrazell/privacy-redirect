afd2a52fc2d2af3dc450a788b4b6fcf7908915b0
NullPenguinException <nullpenguinexception@pm.me>
2021-10-11 21:29:08 +0300

    Update twitter.js

--- src/assets/javascripts/helpers/twitter.js
+++ src/assets/javascripts/helpers/twitter.js
@@ -32,11 +32,12 @@ const redirects = [
   "https://nitter.actionsack.com",
   "https://nitter.namazso.eu",
   "https://nitter.moomoo.me",
-  "https://nitter.exonip.de"
+  "https://nitter.exonip.de",
   "http://3nzoldnxplag42gqjs23xvghtzf6t6yzssrtytnntc6ppc7xxuoneoad.onion",
   "http://nitter.l4qlywnpwqsluw65ts7md3khrivpirse744un3x7mlskqauz5pyuzgqd.onion",
   "http://nitterlgj3n5fgwesu3vxc5h67ruku33nqaoeoocae2mvlzhsu6k7fqd.onion",
   "http://npf37k3mtzwxreiw52ccs5ay4e6qt2fkcs2ndieurdyn2cuzzsfyfvid.onion",
+  "http://nitterrrs6bbcba2bxjviwxzzapkhuuelljtig2ku2rxasweckxxxhid.onion",
 ];
 
 export default {
