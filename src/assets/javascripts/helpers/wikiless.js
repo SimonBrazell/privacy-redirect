c1fb87e4846b3346a86c6dbf52a154a7c2a15469
xanoni <77220130+xanoni@users.noreply.github.com>
2021-10-16 04:38:58 -0400

    Add a 3rd Wikiless instance (sethforprivacy.com)
    New community instance: https://wikiless.sethforprivacy.com/
    
    Plus .onion version!
    
    Via: https://blog.sethforprivacy.com/about/#my-community-resources

--- src/assets/javascripts/helpers/wikiless.js
+++ src/assets/javascripts/helpers/wikiless.js
@@ -3,6 +3,8 @@ const targets = /wikipedia.org/;
 const redirects = [
   "https://wikiless.org",
   "https://wikiless.alefvanoon.xyz",
+  "https://wikiless.sethforprivacy.com",
+  "http://dj2tbh2nqfxyfmvq33cjmhuw7nb6am7thzd3zsjvizeqf374fixbrxyd.onion",
 ];
 
 export default {
