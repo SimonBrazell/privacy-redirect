02d90bda5767e724f9d0077388b835eba880e83f
xanoni <77220130+xanoni@users.noreply.github.com>
2021-10-16 05:02:07 -0400

    Remove Onion v2 links ...
    ... because everything comes to an end

--- src/assets/javascripts/helpers/youtube.js
+++ src/assets/javascripts/helpers/youtube.js
@@ -35,7 +35,6 @@ const redirects = [
   "https://dev.viewtube.io",
   "https://invidious.048596.xyz",
   "http://fz253lmuao3strwbfbmx46yu7acac2jz27iwtorgmbqlkurlclmancad.onion",
-  "http://qklhadlycap4cnod.onion",
   "http://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion",
   "http://w6ijuptxiku4xpnnaetxvnkc5vqcdu7mgns2u77qefoixi63vbvnpnqd.onion",
 ];
