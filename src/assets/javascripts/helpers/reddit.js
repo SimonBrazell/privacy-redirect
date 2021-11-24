d1623e1e781d1145d38a879344bbbe15c4e404a1
Albony Cal <67057319+Albonycal@users.noreply.github.com>
2021-11-24 12:21:15 +0530

    Added libreddit.albony.xyz

--- src/assets/javascripts/helpers/reddit.js
+++ src/assets/javascripts/helpers/reddit.js
@@ -8,6 +8,7 @@ const targets = [
 ];
 const redirects = [
   // libreddit: privacy w/ modern UI
+  "https://libreddit.albony.xyz/",
   "https://libredd.it",
   "https://libreddit.spike.codes",
   "https://libreddit.kavin.rocks",
